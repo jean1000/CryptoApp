4aa68002fa15d76a51f5a7005c80d28af1f56e23
Jean Jures Chevalier <jeanjures@yahoo.fr>
2022-09-02 16:48:07 -0400

    Creation of The Data Class, Dao and ListTypeConverter for the Crypto App

--- app/src/main/java/com/pjp/cryptoapp/data/db/Dao.kt
+++ app/src/main/java/com/pjp/cryptoapp/data/db/Dao.kt
@@ -1,4 +1,13 @@
 package com.pjp.cryptoapp.data.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+
+@Dao
 interface Dao {
+    @Transaction
+    @Query("select * from crypto_table")
+    fun getCryptos(): LiveData<List<Crypto>>
 }
---
+++ app/src/main/java/com/pjp/cryptoapp/data/db/ListTypeConverter.kt
@@ -0,0 +1,26 @@
+package com.pjp.cryptoapp.data.db
+
+import androidx.room.TypeConverter
+
+class ListTypeConverter {
+    companion object{
+        @TypeConverter
+        @JvmStatic
+        fun gettingFloatListFromString(floatList:String?): List<Float>?{
+            val list= arrayListOf<Float>()
+            val array=floatList?.split(",".toRegex())?.dropLastWhile {
+                it.isEmpty()
+            }?.toTypedArray()
+
+            if (array.isNullOrEmpty()){
+                return null
+            }
+            for (s in array){
+               if (s.isNotEmpty()){
+                   list.add(s.toFloat())
+               }
+            }
+            return list
+        }
+    }
+}
